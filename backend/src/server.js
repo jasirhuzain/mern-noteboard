78254722c1662496119a9572802ba5c2a2ac0485
JASIR K A <jasirhuzain@gmail.com>
2026-01-20 23:44:26 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors"
+import dotenv from "dotenv";
+
+import notesRoutes from "./routes/notesRoutes.js"
+import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+
+app.use(cors({
+    orgin:"http://localhost:5173",
+}));
+
+//middleware
+
+app.use(express.json());
+app.use(rateLimiter);
+
+app.use("/api/notes", notesRoutes);
+
+connectDB().then(() => {
+app.listen(PORT ,() => {
+    console.log("server running on PORT:",PORT);
+});
+
+});
+
+
