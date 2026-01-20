a5da6cde93a8a44ddf0d392a052143e26d8acabf
JASIR K A <jasirhuzain@gmail.com>
2026-01-21 00:10:59 +0530

    name change

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
      <header className="bg-base-300 border-b border-base-content/10">
     <div className="mx-auto max-w-6xl p-4">
         <div className="flex items-center justify-between">
-            <h1 className="text-3xl font-bold text-primary font-mono tracking-tighter">Thinkboard</h1>
+            <h1 className="text-3xl font-bold text-primary font-mono tracking-tighter">Noteboard</h1>
             <div className="flex items-center gap-4 ">
             <Link to={"/create"} className="btn btn-primary">
             <Plus className="size-5" />
